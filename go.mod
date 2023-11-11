46197b150eb0b425a848100650c83282f384788c
donghongchen <donghongchen@threatbook.cn>
2023-11-11 23:22:57 +0800

    正则表达式

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/donghc/crawler
 
 go 1.21.0
+
+require (
+	golang.org/x/net v0.18.0 // indirect
+	golang.org/x/text v0.14.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,33 +1,59 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"golang.org/x/net/html/charset"
+	"golang.org/x/text/encoding"
+	"golang.org/x/text/encoding/unicode"
+	"golang.org/x/text/transform"
 	"io/ioutil"
 	"net/http"
+	"regexp"
 )
+
+var headerRe = regexp.MustCompile(`<div class="small_cardcontent__BTALp"[\s\S]*?<h2>([\s\S]*?)</h2>`)
 
 func main() {
 	url := "https://www.thepaper.cn/"
-	resp, err := http.Get(url)
-
-	if err != nil {
-		fmt.Printf("fetch url error:%v \n", err)
-		return
-	}
-
-	defer resp.Body.Close()
-
-	if resp.StatusCode != http.StatusOK {
-		fmt.Printf("Error status code:%v \n", resp.StatusCode)
-		return
-	}
-
-	body, err := ioutil.ReadAll(resp.Body)
+	body, err := Fetch(url)
 
 	if err != nil {
 		fmt.Printf("read content failed:%v \n", err)
 		return
 	}
 
-	fmt.Println("body:", string(body))
+	matches := headerRe.FindAllSubmatch(body, -1)
+	for _, m := range matches {
+		fmt.Println("fetch card news:", string(m[1]))
+	}
+}
+
+func Fetch(url string) ([]byte, error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		panic(err)
+	}
+
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		fmt.Printf("Error status code:%d \n", resp.StatusCode)
+	}
+
+	bodyReader := bufio.NewReader(resp.Body)
+	e := DetermineEncoding(bodyReader)
+	utf8Reader := transform.NewReader(bodyReader, e.NewDecoder())
+
+	return ioutil.ReadAll(utf8Reader)
+}
+
+func DetermineEncoding(r *bufio.Reader) encoding.Encoding {
+	bytes, err := r.Peek(1024)
+	if err != nil {
+		return unicode.UTF8
+	}
+	e, _, _ := charset.DetermineEncoding(bytes, "")
+
+	return e
 }
